5919732d00ab060810cef63546e51f710363704a
Shehan Akalanka <shehan.akalanka@softlogiclife.lk>
2024-11-25 00:16:54 +0530

    git commit check

--- Student.Api/Models/Student.cs
+++ Student.Api/Models/Student.cs
@@ -5,6 +5,6 @@ namespace Student.Api {
     public class Student{
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;// git update check
     }
 }
