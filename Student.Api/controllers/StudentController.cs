101c8f9452fdefa39304faa2f16696fbc4126a01
Shehan Akalanka <shehan.akalanka@softlogiclife.lk>
2024-11-26 08:01:21 +0530

    add student list to the application

--- Student.Api/controllers/StudentController.cs
+++ Student.Api/controllers/StudentController.cs
@@ -2,12 +2,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Student.Api {
     public class StudentController: ControllerBase
-    {
-        private readonly string[] studentList = new[] { "John Doe", "Jane Smith", "Alice Johnson" };
-
+    { private static List<Student> studentList = new List<Student>
+        {
+            new Student { Name = "John Doe", Address = "123 Main St" },
+            new Student { Name = "Jane Smith", Address = "456 Elm St" },
+            new Student { Name = "Emily Johnson", Address = "789 Oak St" }
+        };
+        
         [HttpGet("GetStudentList")]
         public IActionResult GetStudentList(){
-            return Ok(studentList);// with 200 status code
+            return Ok("studentList");// with 200 status code
         }
 
         [HttpPost("CreateStudent")]
